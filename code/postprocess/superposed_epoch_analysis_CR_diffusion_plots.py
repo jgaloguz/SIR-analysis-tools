Fixed bug in diffusion coefficient computation
The computation of the perpendicular diffusion coefficient using UNLT was incorrect. This bug was fixed and the corresponding result plotting routine was updated to reflect this change.

code/postprocess/superposed_epoch_analysis_CR_diffusion_plots.py
@@ -14,7 +14,6 @@ def edge_run_avg(mod_array, org_array, window_size):
 # conversion factors from data to cgs units
 k0_para = 1.0e22
 k0_perp = 1.0e20
-a_perp = 0.45 # Phenomenological constant
 
 # Import SEA analysis results
 GCR1_data = np.loadtxt("output/SEA_cosray_2007-2010.lst")
@@ -33,16 +32,16 @@ GCR2_avgpct = GCR2_data[:,2]
 
 diff_epoch_time = diff1_coeffs_data[:,0]
 diff1_kappa_para_QLT = diff1_coeffs_data[:,1]
-diff1_kappa_perp_FLRW = diff1_coeffs_data[:,2] * a_perp
-diff1_kappa_perp_LZP = diff1_coeffs_data[:,3] * a_perp**2
-diff1_kappa_perp_UNLT = diff1_coeffs_data[:,4] * a_perp**2
+diff1_kappa_perp_FLRW = diff1_coeffs_data[:,2]
+diff1_kappa_perp_LZP = diff1_coeffs_data[:,3]
+diff1_kappa_perp_UNLT = diff1_coeffs_data[:,4]
 diff1_kappa_para_KJ = diff1_coeffs_data[:,5]
 diff1_kappa_para_GJ = diff1_coeffs_data[:,6]
 diff1_kappa_perp_NLGC = diff1_coeffs_data[:,7]
 diff2_kappa_para_QLT = diff2_coeffs_data[:,1]
-diff2_kappa_perp_FLRW = diff2_coeffs_data[:,2] * a_perp
-diff2_kappa_perp_LZP = diff2_coeffs_data[:,3] * a_perp**2
-diff2_kappa_perp_UNLT = diff2_coeffs_data[:,4] * a_perp**2
+diff2_kappa_perp_FLRW = diff2_coeffs_data[:,2]
+diff2_kappa_perp_LZP = diff2_coeffs_data[:,3]
+diff2_kappa_perp_UNLT = diff2_coeffs_data[:,4]
 diff2_kappa_para_KJ = diff2_coeffs_data[:,5]
 diff2_kappa_para_GJ = diff2_coeffs_data[:,6]
 diff2_kappa_perp_NLGC = diff2_coeffs_data[:,7]
@@ -83,7 +82,7 @@ ax2.plot(diff_epoch_time, diff2_kappa_perp_UNLT_plot / k0_perp, 'c', linestyle='
 ax2.set_xlabel('Epoch (days)', fontsize=20)
 ax2.set_ylabel('$\\kappa_\\perp$ ($\\times 10^{20}$ cm$^2$/s)', fontsize=20)
 ax2.set_xlim(-4.0, 4.0)
-ax2.set_ylim(1.5, 4.5)
+ax2.set_ylim(1.5, 6.0)
 ax2.axvline(0.0, color='r', linestyle='--')
 ax2.tick_params(axis='x', labelsize=20)
 ax2.tick_params(axis='y', labelsize=20)
@@ -126,7 +125,7 @@ ax4.plot(diff_epoch_time, diff2_kappa_perp_LZP_plot / k0_perp, 'c', linestyle='-
 ax4.set_xlabel('Epoch (days)', fontsize=20)
 ax4.set_ylabel('$\\kappa_\\perp$ ($\\times 10^{20}$ cm$^2$/s)', fontsize=20)
 ax4.set_xlim(-4.0, 4.0)
-ax4.set_ylim(4.0, 21.0)
+ax4.set_ylim(3.0, 19.0)
 ax4.axvline(0.0, color='r', linestyle='--')
 ax4.tick_params(axis='x', labelsize=20)
 ax4.tick_params(axis='y', labelsize=20)

code/process/compute_diffusion_coefficients.py
@@ -29,6 +29,7 @@ B0 = 5.0e-5
 csc = 1.0 / np.sin(3.0 * np.pi / 5.0)
 k0_para = 1.0e22
 k0_perp = 1.0e20
+a_perp = 0.4
 
 # numerical integration parameters
 Nk = 0
@@ -107,7 +108,7 @@ def integ_kappa_perp_UNLT(B, k_range, k_spect, kappa_para, kappa_perp_init):
    for i in range(maxiter):
       kappa_perp_old = kappa_perp_new
       denom = 4.0 * kappa_perp_old * np.square(k_range) + (v**2 / kappa_para)
-      kappa_perp_new = v**2 / (2.0 * B**2) * sp.integrate.trapezoid(k_spect / denom, k_range)
+      kappa_perp_new = 0.5 * (a_perp * v / B)**2 * sp.integrate.trapezoid(k_spect / denom, k_range)
       if(np.abs(kappa_perp_new - kappa_perp_old) / (np.abs(kappa_perp_new) + np.abs(kappa_perp_old)) < eps):
          break
 
@@ -141,14 +142,13 @@ def kappa_para_QLT_perp_FLRW_LZP_UNLT(folder, n_FD, n_intervals, pct_2D):
             kappa_para_QLT[bin_idx] = kappa_para_QLT[bin_idx] + np.log(kappa_para_QLT_bin)
 # Kappa perpendicular (FLRW)
             kappa2_FL = (0.5 / B**2) * sp.integrate.trapezoid(pct_2D[bin_idx] * k_spect / np.square(k_range), k_range)
-            kappa_perp_FLRW_bin = 0.5 * v * np.sqrt(kappa2_FL)
+            kappa_perp_FLRW_bin = 0.5 * a_perp * v * np.sqrt(kappa2_FL)
             kappa_perp_FLRW[bin_idx] = kappa_perp_FLRW[bin_idx] + np.log(kappa_perp_FLRW_bin)
 # Kappa perpendicular (LZP)
-            kappa_perp_LZP_bin = (0.5 / B**2) * kappa_para_QLT_bin * sp.integrate.trapezoid(pct_2D[bin_idx] * k_spect, k_range)
+            kappa_perp_LZP_bin = 0.5 * (a_perp / B)**2 * kappa_para_QLT_bin * sp.integrate.trapezoid(pct_2D[bin_idx] * k_spect, k_range)
             kappa_perp_LZP[bin_idx] = kappa_perp_LZP[bin_idx] + np.log(kappa_perp_LZP_bin)
 # Kappa perpendicular (UNLT)
-            lambda_para_QLT = 3 * kappa_para_QLT_bin / v
-            kappa_perp_UNLT_bin = integ_kappa_perp_UNLT(B, k_range, pct_2D[bin_idx] * k_spect, lambda_para_QLT, kappa_perp_LZP_bin)
+            kappa_perp_UNLT_bin = integ_kappa_perp_UNLT(B, k_range, pct_2D[bin_idx] * k_spect, kappa_para_QLT_bin, kappa_perp_LZP_bin)
             kappa_perp_UNLT[bin_idx] = kappa_perp_UNLT[bin_idx] + np.log(kappa_perp_UNLT_bin)
 
 # Average over counts